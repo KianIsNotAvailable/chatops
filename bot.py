308d8f493901af2c1aef9d98e6fc11e4b694aff3
KianIsNotAvailable <kianxw@gmail.com>
2023-11-26 11:07:09 +0000

    command

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, request, Response
 from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
@@ -13,6 +13,17 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/eve
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
+message_count = {}
+
+@app.route('/message-count', methods=['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_count.get(user_id, 0)
+    client.chat_postMessage(channel=channel_id, text=f"Message: {message_count}")
+    return Response(), 200
+
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -21,6 +32,10 @@ def message(payload):
     user_id = event.get('user')
     text = event.get('text')
     if BOT_ID != user_id:
+        if user_id in message_count:
+            message_count[user_id] += 1
+        else:
+            message_count[user_id] = 1
         client.chat_postMessage(channel=channel_id, text=text)
 
 
